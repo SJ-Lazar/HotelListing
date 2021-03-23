4d14e6b554d0a12afeb190c951156238790d4dd9
salman lazar <lazarsalman@gmail.com>
2021-03-23 17:52:07 +0200

    Setup SQL Database
    Added DbContext and Creatd Database using Code First

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using HotelListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,11 @@ namespace HotelListing
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("sqlCon"))
+            );
+
+           
 
             //Cors Service
             services.AddCors(c => {
@@ -45,6 +51,8 @@ namespace HotelListing
             });
 
 
+            //Controllers Service
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
