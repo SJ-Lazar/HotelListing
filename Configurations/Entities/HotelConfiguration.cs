964fcde3a1a7d72b462074b6a90f5ed14e8d33ce
salman lazar <lazarsalman@gmail.com>
2021-03-24 02:15:09 +0200

    Abstraction of seed data configuration
    Created Configuration classes for each entity seeded into database.

---
+++ Configurations/Entities/HotelConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using HotelListing.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Configurations.Entities
+{
+    public class HotelConfiguration : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            #region HotelSeedData
+            builder.HasData(
+                       new Hotel
+                       {
+                           Id = 1,
+                           Name = "Sun City Hotel",
+                           Address = "Sun Lane 245",
+                           CountryId = 1,
+                           Rating = 5
+                       },
+                       new Hotel
+                       {
+                           Id = 2,
+                           Name = "Moon City Hotel",
+                           Address = "Moon Lane 105",
+                           CountryId = 2,
+                           Rating = 4
+                       },
+                       new Hotel
+                       {
+                           Id = 3,
+                           Name = "Mars City Hotel",
+                           Address = "Mars Lane 5",
+                           CountryId = 3,
+                           Rating = 3.5
+                       }); 
+            #endregion
+        }
+    }
+}
--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HotelListing.Configurations.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -16,54 +17,9 @@ namespace HotelListing.Data
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<Country>()
-                            .HasData(new Country
-                            {
-                                Id = 1,
-                                Name ="South Africa",
-                                ShortName = "RSA"
-                            },
-                            new Country
-                            {
-                                Id = 2,
-                                Name = "Canada",
-                                ShortName = "CA"
-                            },
-                            new Country
-                            {
-                                Id= 3,
-                                Name = "United States Of America",
-                                ShortName = "USA"
-                            }
-                );
-
-            builder.Entity<Hotel>()
-                           .HasData(new Hotel
-                           {
-                               Id = 1,
-                               Name = "Sun City Hotel",
-                               Address = "Sun Lane 245",
-                               CountryId = 1,
-                               Rating = 5
-
-                           },
-                           new Hotel
-                           {
-                               Id = 2,
-                               Name = "Moon City Hotel",
-                               Address = "Moon Lane 105",
-                               CountryId = 2,
-                               Rating = 4
-                           },
-                           new Hotel
-                           {
-                               Id = 3,
-                               Name = "Mars City Hotel",
-                               Address = "Mars Lane 5",
-                               CountryId = 3,
-                               Rating = 3.5
-                           }
-               );
+            builder.ApplyConfiguration(new RoleConfiguration());
+            builder.ApplyConfiguration(new CountryConfiguration());
+            builder.ApplyConfiguration(new HotelConfiguration()); 
         }
     }
 }
